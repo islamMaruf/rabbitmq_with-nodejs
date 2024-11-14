4b6df1fcb75d410d5c8550c66db2ad5191d9743e
maruf.islam <maruf.islam@miaki.co>
2024-11-14 15:16:03 +0600

    feat: get env from env

--- config.js
+++ config.js
@@ -1,16 +1,5 @@
-// RABBITMQ_HOST=armadillo.rmq.cloudamqp.com
-// RABBITMQ_PORT=5671
-// RABBITMQ_USERNAME=ngcwvdbn
-// RABBITMQ_PASSWORD=
-// RABBITMQ_VHOST=ngcwvdbn
-// RABBITMQ_MESSAGE_LIFETIME=10000
-// RABBITMQ_CONSUMER_SLEEP_TIME=60
-
-const host = "armadillo.rmq.cloudamqp.com";
-const port = 5672;
-const username = "ngcwvdbn";
-const password = "KZkjfw7KNRz_rQHqfe7UCquSHXM6IALC";
+require('dotenv').config();
 
 module.exports = {
-    base_url : `amqps://ngcwvdbn:KZkjfw7KNRz_rQHqfe7UCquSHXM6IALC@armadillo.rmq.cloudamqp.com/ngcwvdbn`
-}
+    base_url: `amqps://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}/${process.env.RABBITMQ_VHOST}`
+};
